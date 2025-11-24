d57a48d8c0f507128773469188c72ae780f62923
Owen Le <Owen.TaiVan.Le@ibm.com>
2025-11-24 19:26:33 +0000

    edit: changes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,11 +1,11 @@
 /* Description: Custom JS file */
 
-(function($) {
+(function ($) {
     "use strict";
 
     /* Navbar Scripts */
     // Collapse navbar on scroll
-    $(window).on('scroll load', function() {
+    $(window).on('scroll load', function () {
         if ($(".navbar").offset().top > 60) {
             $(".fixed-top").addClass("top-nav-collapse");
         } else {
@@ -14,8 +14,8 @@
     });
 
     // Smooth scrolling (requires jQuery Easing)
-    $(function() {
-        $(document).on('click', 'a.page-scroll', function(event) {
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top
@@ -24,13 +24,20 @@
         });
     });
 
-    // Offcanvas behavior for small screens (optional animation class)
-    $('[data-bs-toggle="collapse"], .navbar-nav li a:not(.dropdown-toggle)').on('click', function () {
+    // Offcanvas behavior for small screens
+    // Only trigger from the navbar toggle button
+    $('.navbar-toggler').on('click', function () {
         $('.offcanvas-collapse').toggleClass('open');
     });
 
+    // When clicking a nav link (except dropdown toggles), close the menu
+    $('.navbar-nav li a:not(.dropdown-toggle)').on('click', function () {
+        $('.offcanvas-collapse').removeClass('open');
+    });
+
+
     /* Move Form Fields Label When User Types */
-    $("input, textarea").keyup(function() {
+    $("input, textarea").keyup(function () {
         if ($(this).val() !== '') {
             $(this).addClass('notEmpty');
         } else {
@@ -42,7 +49,7 @@
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > amountScrolled) {
             $('a.back-to-top').fadeIn('500');
         } else {
@@ -51,7 +58,7 @@
     });
 
     /* Removes Long Focus On Buttons */
-    $(".button, a, button").mouseup(function() {
+    $(".button, a, button").mouseup(function () {
         $(this).blur();
     });
 
